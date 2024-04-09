8ac7d6d72c5c1e6f93b203ed94174c09282e735e
Zonggui Chen <ggchenzonggui@qq.com>
2024-04-09 21:57:32 +0800

    Added download linkers for PB-CCS and ONT-UL bams

---
+++ public/GIAB/HG001_GRCh38_NISTv4.2.1/PacBio_SequelII_CCS_11kb/00_download.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/NA12878/PacBio_SequelII_CCS_11kb/HG001_GRCh38/HG001_GRCh38.haplotag.RTG.trio.bam
+wget https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/NA12878/PacBio_SequelII_CCS_11kb/HG001_GRCh38/HG001_GRCh38.haplotag.RTG.trio.bam.bai
---
+++ public/GIAB/HG001_GRCh38_NISTv4.2.1/Ultralong_OxfordNanopore/00_download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/NA12878/Ultralong_OxfordNanopore/NA12878-minion-ul_GRCh38.bam
+wget https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/NA12878/Ultralong_OxfordNanopore/NA12878-minion-ul_GRCh38.bam.bai
